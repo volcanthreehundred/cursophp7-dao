f80589ac0002a0fcce85a7101011bd038060dc75
volcanthreehundred <volcanthreehundred@gmail.com>
2020-08-03 17:09:09 -0300

    Criamos a classe usuario, corrigimos bug na class sql

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 
 	require_once("config.php");
 
-	$sql = new Sql();
+	$volcan = new Usuario();
 
-	$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+	$volcan->loadById(10);
 
-	echo json_encode($usuarios);
+	echo $volcan;
 
 ?>
